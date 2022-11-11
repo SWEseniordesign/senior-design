63f7cb49d32e0423291adbd0cc6259d7a70a43fb
bflemin1 <bflemin1@unb.ca>
2022-11-11 17:24:35 -0400

    Backend routing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const Item = require('./models/Item');
+var cors = require('cors');
 
 // Load config
 dotenv.config();
@@ -9,10 +11,20 @@ connectDB();
 
 const app = express();
 
+// Cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Middleware
+app.use(express.json());
+
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+// Routes
+app.use('/items', require('./routes/items'));
 
 const PORT = process.env.PORT || 5000;
 
