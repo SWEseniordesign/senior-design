da1eb52921c079a94978c583b62e66210d489ed8
Grace Kiely <gkkiely11@gmail.com>
2022-11-12 21:56:19 -0400

    started to add sections to lp css not rly working

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,8 @@
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
+import "./Home.css";
+import { COLOR_PALETTE } from "../../Constants";
 
 const useStyle = makeStyles({
     root: {
@@ -14,8 +16,17 @@ export const Home = () => {
     const classes = useStyle();
 
     return (
-        <div className={classes.root}>
-            <Typography sx={{fontSize: '60px'}}>Welcome to my-till!</Typography>
+        <div className={classes}>
+            <div className={classes.singleSectionBox}>
+                <Typography sx={{fontSize: '60px'}}>Welcome to my-till!</Typography>
+                <div className={classes.buttonBox}>
+                    <Button variant="contained" sx={{background: COLOR_PALETTE.NAVY_BLUE}}>SIGN UP</Button>
+                    <Button variant="outlined" sx={{color: COLOR_PALETTE.NAVY_BLUE, outlineColor: COLOR_PALETTE.NAVY_BLUE}}>LOG IN</Button>
+                </div>
+            </div>
+            <div className={classes.steps}>
+                
+            </div>
         </div>
     );
 }
