412d851ad6680a36029de9559e5065d44d01cada
j-savoie <jacob.savoie@ymail.com>
2022-11-19 14:32:56 -0400

    remove testing code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import { Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
-import MTButton from "../../components/mui/MTButton"
 import React from "react";
 
 const useStyle = makeStyles({
@@ -16,7 +15,6 @@ export const Home = () => {
 
     return (
         <div className={classes.root}>
-            <MTButton variant = {"outlined"} label = {"balls"}></MTButton>
             <Typography sx={{fontSize: '60px'}}>Welcome to my-till!</Typography>
         </div>
     );
