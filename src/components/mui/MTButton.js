e02c8d5cb9888a7f92f966bf2bf0c254a1041906
j-savoie <jacob.savoie@ymail.com>
2022-11-20 15:43:40 -0400

    add theme and working colored switch

--- src/components/mui/MTButton.js
+++ src/components/mui/MTButton.js
@@ -1,22 +1,11 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { makeStyles } from "@mui/styles";
-import { COLOR_PALETTE } from "../../Constants";
 
 const useStyles = makeStyles({
   MTButton: {
     height: '50px',
-    '&.MuiButtonBase-root':{
-        borderColor:  COLOR_PALETTE.BLUE_GROTTO,
-        color: props => props.variant !== 'outlined' ? 'white' : COLOR_PALETTE.BLUE_GROTTO,
-        backgroundColor: props => props.variant !== 'outlined' ? COLOR_PALETTE.BLUE_GROTTO : 'white',
-        '&:hover': {
-          borderColor:  COLOR_PALETTE.NAVY_BLUE,
-          color: props => props.variant !== 'outlined' ? 'white' : COLOR_PALETTE.NAVY_BLUE,
-          backgroundColor: props => props.variant !== 'outlined' ? COLOR_PALETTE.NAVY_BLUE : 'lightgray',
-      }
-    }
-},
+  }
 })
 export default function BasicButtons(props) {    
   const classes = useStyles(props);
