b68d5de633ecd602167a49ce288242783b9dfbcd
Colby <cfoster3@unb.ca>
2023-01-24 13:41:30 -0400

    Finalizes connecting to myTill or test dbs

--- server/config/db.js
+++ server/config/db.js
@@ -2,26 +2,23 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        console.log('Connecting to Database...');
-        console.log(`Script ran: ${process.env.npm_lifecycle_event}`);
-        if(process.env.npm_lifecycle_event === 'dev'){
-            const conn = await mongoose.connect(process.env.MONGO_URI_TEST, {
-                useNewUrlParser: true, 
-                useUnifiedTopology: true
-            });
-            console.log('DB: test');
-            console.log(`MongoDB Connected: ${conn.connection.host}`);
-        }
-        else {
+        //if yarn server was ran, connect to myTill
+        if(process.env.npm_lifecycle_event === 'server'){
+            console.log('Connecting to Database: myTill');
             const conn = await mongoose.connect(process.env.MONGO_URI_MYTILL, {
                 useNewUrlParser: true, 
                 useUnifiedTopology: true
             });
-            console.log('DB: myTill');
             console.log(`MongoDB Connected: ${conn.connection.host}`);
         }
-        
-        //console.log(`MongoDB Connected: ${conn.connection.host}`);
+        else { //else connect to test
+            console.log('Connecting to Database: test');
+            const conn = await mongoose.connect(process.env.MONGO_URI_TEST, {
+                useNewUrlParser: true, 
+                useUnifiedTopology: true
+            });
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
+        }
     } catch (err) {
         console.error(err);
         process.exit(1);
