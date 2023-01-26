06a8489aea77bf520da2480173e48f5e6fa84798
Colby <cfoster3@unb.ca>
2023-01-26 12:55:02 -0400

    Updates to user get business tests

--- server/__tests__/User/business.js
+++ server/__tests__/User/business.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 afterAll(() => mongoose.disconnect(), 10000);
 
 describe('POST /business', () => {
-
     it('should return 201 and true if valid credentials are sent & user has business', async () => {
         const userData = { email: 'dcampb13@unb.ca', password: 'mainaccount' }; 
 
@@ -20,6 +19,25 @@ describe('POST /business', () => {
             .set('authorization', login.body.token) 
             .expect(201)
             .send()
-        expect(business).toBe({business: true, code: 201});  
-    });  
+        expect(business._body.business).toBe(true);
+        expect(business._body.code).toBe(201);
+    });
+
+    it('should return 201 and false if valid credentials are sent & user does not have a business', async () => {
+        const userData = { email: 'jimmy@joe.j', password: 'jimmy' }; 
+
+        const login = await request(app)
+            .post('/user/login')
+            .expect(200)
+            .send(userData) 
+        expect(login.body.token).toBeDefined();  
+
+        const business = await request(app)
+            .post('/user/business')
+            .set('authorization', login.body.token) 
+            .expect(201)
+            .send()
+        expect(business._body.business).toBe(false);
+        expect(business._body.code).toBe(201);
+    }); 
 });
