45e167d8a65bd8c3856a81d622a085cb5c87bab9
Ben Fleming <bflemin1@unb.ca>
2023-02-07 20:32:55 -0400

    Buttons and text alignment

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,6 @@
-import { Paper, Card, Typography, Box } from "@mui/material";
+import { Paper, Card, Typography, Box, Fab, IconButton } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Grid2 from "@mui/material/Unstable_Grid2";
 import { makeStyles } from "@mui/styles";
 import { COLOR_PALETTE, FONT_FAMILY } from "../../Constants";
@@ -30,8 +32,7 @@ const useStyle = makeStyles({
     widget: {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'left',
         height: '100%',
         width: '100%',
     },
@@ -43,23 +44,27 @@ export const Dashboard = () => {
 
     return (
         <div className={classes.root}>
-            <Paper className={classes.board} elevation={5} sx={{
+            {/* <Paper className={classes.board} elevation={5} sx={{
                 backgroundColor: COLOR_PALETTE.BABY_BLUE,
-            }}>
+            }}> */}
                 <div className={classes.container}>
                     <Grid2 container sx={{height: '100%', width: '100%'}} spacing={2}>
                         <Grid2 id='grid-panel-left' xs={12} md={5} sx={{height: '100%'}}>
                             <Grid2 container sx={{height: '100%', width: '100%', position: 'relative'}}>
                                 <Grid2 id='grid-left-top' xs={12} md={12} sx={{position: 'absolute', top:0, left: 0, height: '40%'}}>
-                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BLUE_GROTTO}} elevation={3}>
-                                        <Typography variant='h4'> Title </Typography>
-                                        <Typography variant='subtitle1'> Subtitle </Typography>
+                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BABY_BLUE}} elevation={3}>
+                                        <Box ml={4} mt={8}>
+                                            <Typography variant='h4'> Title </Typography>
+                                            <Typography variant='subtitle1'> Subtitle </Typography>
+                                        </Box>
                                     </Card>
                                 </Grid2>
                                 <Grid2 id='grid-left-bottom' xs={12} md={12} sx={{position: 'absolute', bottom: 0, left: 0, height: '60%'}}>
-                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BLUE_GROTTO}} elevation={3}>
-                                        <Typography variant='h4'> Title2</Typography>
-                                        <Typography variant='subtitle1'> Subtitle2 </Typography>
+                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BABY_BLUE}} elevation={3}>
+                                        <Box ml={4} mt={8}>
+                                            <Typography variant='h4'> Title2</Typography>
+                                            <Typography variant='subtitle1'> Subtitle2 </Typography>
+                                        </Box>
                                     </Card>
                                 </Grid2>
                             </Grid2>
@@ -67,16 +72,24 @@ export const Dashboard = () => {
                         <Grid2 id='grid-panel-right' xs={12} md={7} sx={{height: '100%'}}>
                             <Grid2 container sx={{height: '100%', width: '100%', position: 'relative', display: 'flex', alignItems: 'center'}}>
                                 <Grid2 id='grid-right' xs={12} md={12} sx={{position: 'absolute', right: 0, height: '100%',}}>
-                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BLUE_GROTTO}} elevation={3}>
-                                        <Typography variant='h4'> Title3</Typography>
-                                        <Typography variant='subtitle1'> Subtitle3 </Typography>
+                                    <Card className={classes.widget} sx={{backgroundColor: COLOR_PALETTE.BABY_BLUE, position: 'relative'}} elevation={3}>
+                                        <Box ml={4} mt={8}>
+                                            <Typography variant='h4'> Title3</Typography>
+                                            <Typography variant='subtitle1'> Subtitle3 </Typography>
+                                            <Fab color="primary" aria-label="add" sx={{position: 'absolute', bottom: 20, right: 20}}>
+                                                <AddIcon />
+                                            </Fab>
+                                            <IconButton aria-label="more" sx={{position: 'absolute', top: 20, right: 20}}>
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                        </Box>
                                     </Card>
                                 </Grid2>
                             </Grid2>
                         </Grid2>
                     </Grid2>
                 </div>
-            </Paper>
+            {/* </Paper> */}
         </div>
     );
 }
