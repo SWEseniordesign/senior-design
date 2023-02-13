cbf5ba12097bb43f7a8448e49b515f042042eac3
Ben Fleming <bflemin1@unb.ca>
2023-02-13 16:23:25 -0400

    Fix for dashboard till backend req

--- src/requests/tills-req.js
+++ src/requests/tills-req.js
@@ -46,24 +46,6 @@ export async function getTill (obj) {
 }
 
 /*
-Get all Tills from a business
-*/
-export async function getAllTills (obj) {
-    let data;
-    await fetch('http://localhost:8080/till/getAll', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'authorization': userState.token.get()
-        },
-        body: JSON.stringify(obj)
-    })
-    .then(res => data = res.json())
-    .catch(err => console.log(err));
-    return data;
-}
-
-/*
 modify a Till's employees
 */
 export async function addEmployees (obj) {
