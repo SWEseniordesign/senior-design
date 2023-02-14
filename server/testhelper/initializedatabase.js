944e87dbafee8ebb2ed5ebd4d9a686bf76daba6f
ColbyFoster3 <cfoster3@unb.ca>
2023-02-14 12:39:34 -0400

    Adds support to run new yarn script 'initdb'

--- server/testhelper/initializedatabase.js
+++ server/testhelper/initializedatabase.js
@@ -1,10 +1,38 @@
 const {testUser, testUser1, testUserBusId} = require('./variables');
 const request = require('supertest');
 const app = require('../server');
+const mongoose = require('mongoose');
+//const disconnectDB = require('../config/db');
+if(process.env.npm_lifecycle_event === 'dbscript'){
+    start();
+}
+
+async function start(){
+    await initializeDatabase();
+     try{
+         await mongoose.disconnect();
+         console.log('Disconnecting from database...');
+        if(mongoose.connection.readyState === 0){
+            console.log("Database connection successfully closed.");
+        }
+        else {
+            console.log('Error closing database connection, closing process...');
+            process.exit(1); 
+        }
+        
+        console.log('Exiting process...');
+        process.exit(0);
+    }
+    catch(err){
+        console.error(err);
+        process.exit(1);
+    }
+}
+
 
 async function initializeDatabase(){
     try{
-        const user1 = await request(app)
+        /*const user1 = await request(app)
             .post('/user/register')
             .expect(201)
             .send(testUser);
@@ -17,10 +45,11 @@ async function initializeDatabase(){
         const user3 = await request(app)
             .post('/user/register')
             .expect(201)
-            .send(testUserBusId);
+            .send(testUserBusId);*/
     }
     catch(e){
         return {err: 'Error creating users'};
     }
 }
+
 module.exports = initializeDatabase;
