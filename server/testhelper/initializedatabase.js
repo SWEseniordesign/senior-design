f9af2f64609411021f601c11539d298736bebc25
Colby <cfoster3@unb.ca>
2023-03-07 10:36:17 -0400

    Updates initdb to include more data

--- server/testhelper/initializedatabase.js
+++ server/testhelper/initializedatabase.js
@@ -1,4 +1,9 @@
-const {testUser, testUser1, testUser2, testUser3, testUserBusId, testBusiness, testTill, testTab, testCard, testItem} = require('./variables');
+const {testUser, testUser1, testUser2, testUser3, testUserBusId, testTill2, 
+    testBusiness, testTill, testTabPizza, testCardClassicPizza, testItem1CP, testItem2CP, 
+    testCardSpecialityPizza, testItem1SP, testItem2SP,
+    testTabSandwiches, testCardBurgers, testItem1B, testItem2B,
+    testCardClassicSand, testItem1C, testItem2C
+} = require('./variables');
 const request = require('supertest');
 const app = require('../server');
 const mongoose = require('mongoose');
@@ -71,52 +76,200 @@ async function initializeDatabaseNewScript(){
     }
 
     testTill.businessId = businessUser1._body.formattedBus.id;
-    const tillUser1 = await request(app)
+    const till1User1 = await request(app)
         .post('/till/create')
         .set('authorization', loginUser1.body.token) 
         //.expect(201)
         .send(testTill);
-    if(tillUser1._body.err != null){
-        console.log(`\tError creating Till: ${tillUser1._body.err}.`);
+    if(till1User1._body.err != null){
+        console.log(`\tError creating Till: ${till1User1._body.err}.`);
         console.log('\tStopping data creation...');
         return;
     }
 
-    testTab.tillId = tillUser1._body.formattedTill.id;
-    const tabUser1 = await request(app)
+    testTill2.businessId = businessUser1._body.formattedBus.id;
+    const till2User1 = await request(app)
+        .post('/till/create')
+        .set('authorization', loginUser1.body.token) 
+        //.expect(201)
+        .send(testTill2);
+    if(till2User1._body.err != null){
+        console.log(`\tError creating Till: ${till2User1._body.err}.`);
+        console.log('\tStopping data creation...');
+        return;
+    }
+
+    //Creates a Tab called Pizza for till1User1
+    testTabPizza.tillId = till1User1._body.formattedTill.id;
+    const tabPizzaUser1 = await request(app)
         .post('/tab/create')
         .set('authorization', loginUser1.body.token) 
         //.expect(201)
-        .send(testTab);
-    if(tabUser1._body.err != null){
-        console.log(`\tError creating Tab: ${tabUser1._body.err}.`);
+        .send(testTabPizza);
+    if(tabPizzaUser1._body.err != null){
+        console.log(`\tError creating Tab: ${tabPizzaUser1._body.err}.`);
         console.log('\tStopping data creation...');
         return;
     }
+        //Creates a Card called Classic for Pizza Tab
+        testCardClassicPizza.tabId = tabPizzaUser1._body.formattedTab.id;
+        const cardCPUser1 = await request(app)
+            .post('/card/create')
+            .set('authorization', loginUser1.body.token) 
+            //.expect(201)
+            .send(testCardClassicPizza);
+        if(cardCPUser1._body.err != null){
+            console.log(`\tError creating Card: ${cardCPUser1._body.err}.`);
+            console.log('\tStopping data creation...');
+            return;
+        }   
+            //Creates a item for the Classic Tab
+            testItem1CP.cardId = cardCPUser1._body.formattedCard.id;
+            const itemCP1User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem1CP);
+            if(itemCP1User1._body.err != null){
+                console.log(`\tError creating Item: ${itemCP1User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+            //Creates a item for the Classic Tab
+            testItem2CP.cardId = cardCPUser1._body.formattedCard.id;
+            const itemCP2User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem2CP);
+            if(itemCP2User1._body.err != null){
+                console.log(`\tError creating Item: ${itemCP2User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+        //Creates a Card called Speciality for Pizza Tab
+        testCardSpecialityPizza.tabId = tabPizzaUser1._body.formattedTab.id;
+        const cardSPUser1 = await request(app)
+            .post('/card/create')
+            .set('authorization', loginUser1.body.token) 
+            //.expect(201)
+            .send(testCardSpecialityPizza);
+        if(cardSPUser1._body.err != null){
+            console.log(`\tError creating Card: ${cardSPUser1._body.err}.`);
+            console.log('\tStopping data creation...');
+            return;
+        }
+            //Creates a item for the Speciality Card
+            testItem1SP.cardId = cardSPUser1._body.formattedCard.id;
+            const itemSP1User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem1SP);
+            if(itemSP1User1._body.err != null){
+                console.log(`\tError creating Item: ${itemSP1User1._body.err}.`);
+                console.log(`\tcardId: ${testItem1SP.cardId}`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+            //Creates a item for the Speciality Card
+            testItem2SP.cardId = cardSPUser1._body.formattedCard.id;
+            const itemSP2User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem2SP);
+            if(itemSP2User1._body.err != null){
+                console.log(`\tError creating Item: ${itemSP2User1._body.err}.`);
+                console.log(`\tcardId: ${testItem1SP.cardId}`);
+                console.log('\tStopping data creation...');
+                return;
+            }
 
-    testCard.tabId = tabUser1._body.formattedTab.id;
-    const cardUser1 = await request(app)
-        .post('/card/create')
+    //Creates a Tab called Sandwiches for till1User1
+    testTabSandwiches.tillId = till1User1._body.formattedTill.id;
+    const tabSandwichesUser1 = await request(app)
+        .post('/tab/create')
         .set('authorization', loginUser1.body.token) 
         //.expect(201)
-        .send(testCard);
-    if(cardUser1._body.err != null){
-        console.log(`\tError creating Card: ${cardUser1._body.err}.`);
+        .send(testTabSandwiches);
+    if(tabSandwichesUser1._body.err != null){
+        console.log(`\tError creating Tab: ${tabSandwichesUser1._body.err}.`);
         console.log('\tStopping data creation...');
         return;
     }
-
-    testItem.cardId = cardUser1._body.formattedCard.id;
-    const itemUser1 = await request(app)
-        .post('/items/create')
-        .set('authorization', loginUser1.body.token) 
-        //.expect(201)
-        .send(testItem);
-    if(itemUser1._body.err != null){
-        console.log(`\tError creating Item: ${itemUser1._body.err}.`);
-        console.log('\tStopping data creation...');
-        return;
-    }
+        //Creates a Card called Burgers for Sandwiches Tab
+        testCardBurgers.tabId = tabSandwichesUser1._body.formattedTab.id;
+        const cardBurgersUser1 = await request(app)
+            .post('/card/create')
+            .set('authorization', loginUser1.body.token) 
+            //.expect(201)
+            .send(testCardBurgers);
+        if(cardBurgersUser1._body.err != null){
+            console.log(`\tError creating Card: ${cardBurgersUser1._body.err}.`);
+            console.log('\tStopping data creation...');
+            return;
+        }
+            //Creates a Item for Burgers Card
+            testItem1B.cardId = cardBurgersUser1._body.formattedCard.id;
+            const itemB1User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem1B);
+            if(itemB1User1._body.err != null){
+                console.log(`\tError creating Item: ${itemB1User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+            //Creates a Item for Burgers Card
+            testItem2B.cardId = cardBurgersUser1._body.formattedCard.id;
+            const itemB2User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem2B);
+            if(itemB2User1._body.err != null){
+                console.log(`\tError creating Item: ${itemB2User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+        //Creates a Card called Classic for Sandwiches Tab
+        testCardClassicSand.tabId = tabSandwichesUser1._body.formattedTab.id;
+        const cardClassicSandUser1 = await request(app)
+            .post('/card/create')
+            .set('authorization', loginUser1.body.token) 
+            //.expect(201)
+            .send(testCardClassicSand);
+        if(cardClassicSandUser1._body.err != null){
+            console.log(`\tError creating Card: ${cardClassicSandUser1._body.err}.`);
+            console.log('\tStopping data creation...');
+            return;
+        }
+            //Creates a Item for Classic Card
+            testItem1C.cardId = cardClassicSandUser1._body.formattedCard.id;
+            const itemC1User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem1C);
+            if(itemC1User1._body.err != null){
+                console.log(`\tError creating Item: ${itemC1User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
+            //Creates a Item for Classic Card
+            testItem2C.cardId = cardClassicSandUser1._body.formattedCard.id;
+            const itemC2User1 = await request(app)
+                .post('/items/create')
+                .set('authorization', loginUser1.body.token) 
+                //.expect(201)
+                .send(testItem2C);
+            if(itemC2User1._body.err != null){
+                console.log(`\tError creating Item: ${itemC2User1._body.err}.`);
+                console.log('\tStopping data creation...');
+                return;
+            }
     console.log('\tData created successfully.');
 }
 
--- server/testhelper/variables.js
+++ server/testhelper/variables.js
@@ -39,8 +39,8 @@ let testUserBusId = {
 };
 
 let testBusiness = {
-    name: 'Test Business 1',
-    type: 'Wholesale',
+    name: 'We Da Best Food',
+    type: 'Quick Service',
     admins: [],
     tills: []
 };
@@ -66,47 +66,121 @@ let testBusinessType2 = {
     tills: ['yoyoyoyoyoyo']
 };
 
+
 let testTill = {
-    name: 'Test Location 1',
+    name: 'Fredericton North',
     managerPassword: 123,
     employees: [],
     tabs: [],
     props: [],
     businessId: ''
 };
+    let testTabPizza = {
+        name: 'Pizza',
+        color: '',
+        cards: []
+    };
+        let testCardClassicPizza = {
+            name: 'Classic',
+            color: 'yellow',
+            dimensions: {x: null, y: null, width: null, height: null},
+            items: [],
+            tabId: ''
+        };
+            let testItem1CP = {
+                name: 'Pepperoni',
+                price: 19.99,
+                image: '',
+                props: [],
+                stock: 45
+            };
+
+            let testItem2CP = {
+                name: 'Meat Lover\'s',
+                price: 20.99,
+                image: '',
+                props: [],
+                stock: 37
+            };
+        let testCardSpecialityPizza = {
+            name: 'Speciality',
+            color: 'yellow',
+            dimensions: {x: null, y: null, width: null, height: null},
+            items: [],
+            tabId: ''
+        };
+            let testItem1SP = {
+                name: 'Smokey Maple Bacon',
+                price: 24.99,
+                image: '',
+                props: [],
+                stock: 45
+            };
+            let testItem2SP = {
+                name: 'Margherita',
+                price: 24.99,
+                image: '',
+                props: [],
+                stock: 69
+            };
+
+    let testTabSandwiches = {
+        name: 'Sandwiches',
+        color: '',
+        cards: []
+    };
+        let testCardBurgers = {
+            name: 'Burgers',
+            color: 'red',
+            dimensions: {x: null, y: null, width: null, height: null},
+            items: [],
+            tabId: ''
+        };
+            let testItem1B = {
+                name: 'BigMac',
+                price: 69.09,
+                image: '',
+                props: [],
+                stock: 4
+            };
+
+            let testItem2B = {
+                name: 'Baconator',
+                price: 15.00,
+                image: '',
+                props: [],
+                stock: 100
+            };
+        let testCardClassicSand = {
+            name: 'Classic',
+            color: 'blue',
+            dimensions: {x: null, y: null, width: null, height: null},
+            items: [],
+            tabId: ''
+        };
+            let testItem1C = {
+                name: 'Club Sandwich',
+                price: 69.09,
+                image: '',
+                props: [],
+                stock: 4
+            };
+            let testItem2C = {
+                name: 'Crispy Chicken Sandwich',
+                price: 15.00,
+                image: '',
+                props: [],
+                stock: 100
+            };
+
 
 let testTill2 = {
-    name: '123 @ Test Street',
+    name: 'Fredericton South(No Tabs/cards/items)',
     managerPassword: 123,
     employees: [],
     tabs: [],
     props: [],
     businessId: ''
-};
-
-let testTab = {
-    name: 'Sandwiches',
-    managerPassword: 123,
-    employees: [],
-    tabs: [],
-    props: [],
-    tillId: ''
-};
-
-let testCard = {
-    name: 'Burgers',
-    color: 'red',
-    dimensions: {x: null, y: null, width: null, height: null},
-    items: [],
-    tabId: ''
-};
-
-let testItem = {
-    name: 'BigMac',
-    price: 69.09,
-    image: '',
-    props: [],
-    stock: 4
 };
 
 let testEmployee = {
@@ -130,9 +204,20 @@ module.exports = {
     testBusinessType2,
     testTill,
     testTill2,
-    testTab,
-    testCard,
-    testItem,
+    testTabPizza,
+    testCardClassicPizza,
+    testItem1CP,
+    testItem2CP,
+    testCardSpecialityPizza,
+    testItem1SP,
+    testItem2SP,
+    testTabSandwiches,
+    testCardBurgers,
+    testItem1B,
+    testItem2B,
+    testCardClassicSand,
+    testItem1C,
+    testItem2C,
     testEmployee,
     fakeObjectIdType1,
     fakeObjectIdType2,
