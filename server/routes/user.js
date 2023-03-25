f96df99acd630624f99d9f0ba1f35ebf6f5b29e1
Duncan Campbell <duncan.campbell@unb.ca>
2023-03-25 12:49:32 -0300

    g

--- server/routes/user.js
+++ server/routes/user.js
@@ -17,10 +17,11 @@ const {verifyJWTOwner} = require('../middleware/auth');
  *        500 Internal Server Error
  */
 router.post('/login', async function(req, res) {
-    //Check if there is a body in the request
-    if(Object.keys(req.body).length === 0) return res.status(400).send({err: 'No request body', code: 400});
 
     return req;
+
+    //Check if there is a body in the request
+    if(Object.keys(req.body).length === 0) return res.status(400).send({err: 'No request body', code: 400});
 
     //find the user
     let find_user = await User.findOne({email: req.body.email}).exec().catch( err => {return res.status(500).send({err: 'Internal Server Error', code: 500})});
