c73b3af4a8face1248c5c3805e2f2362a459a8f9
Duncan Campbell <duncan.campbell@unb.ca>
2023-03-29 18:09:36 -0300

    Fixed warnings

--- src/components/drawer/CartDrawer.js
+++ src/components/drawer/CartDrawer.js
@@ -33,7 +33,6 @@ const useStyles = makeStyles({
 const CartDrawer = () => {
 
   const localOrderState = useHookstate(orderState);
-  const [err, setErr] = useState('');
 
   const addItemToCart = (item) => {
     let itemIndex = localOrderState.order.get().findIndex((i) => i.id === item.id);
@@ -78,7 +77,7 @@ const CartDrawer = () => {
     let transactionResponse = await createTransaction(newTransaction);
     console.log(!!(transactionResponse.err))
     if(!!(transactionResponse.err)){
-      setErr(transactionResponse.err);
+      console.log(transactionResponse.err);
     } else {
       localOrderState.order.set([]);
     }
